55ca55169529a788ef438295e033edcb83b5bdd6
Goran Žuri <goran.zuri@gmail.com>
2014-02-20 15:18:34 +0100

    Added image from base64

---
+++ SimplHelpers/SimplHelpers/ImageHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SimplHelpers
+{
+    public static class ImageHelpers
+    {
+        public static Image GetImageFromBase64(string imageInBase64)
+        {
+            //get a temp image from bytes, instead of loading from disk
+            //data:image/gif;base64,
+            //this image is a single pixel (black)
+            byte[] bytes = Convert.FromBase64String(imageInBase64);
+
+            Image image;
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                image = Image.FromStream(ms);
+            }
+
+            return image;
+        }
+    }
+}
