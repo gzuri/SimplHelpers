be96df3f5d1c394a665a282c6edca7072281b82e
Goran Žuri <goran.zuri@gmail.com>
2014-02-20 14:03:25 +0100

    Added enum to dictionary

---
+++ SimplHelpers/SimplHelpers.Test/CollectionsHelpersTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplHelpers;
+using System.Collections.Generic;
+namespace SimplHelpers.Test
+{
+    [TestClass]
+    public class CollectionsHelpersTest
+    {
+        [TestMethod]
+        public void SerializeToDictionary()
+        {
+            var enumDictionary = CollectionsHelpers.EnumToDictionary(CurrencyEnum.EUR);
+            Assert.AreEqual(enumDictionary.Count, 2, "All enum values didn't serialize");
+            string eur;
+            Assert.IsTrue(enumDictionary.TryGetValue(978, out eur), "Values didn't serialize correctly");
+        }
+
+        public enum CurrencyEnum 
+        { 
+            EUR = 978,
+            USD = 840,
+            USaD = 840
+        }
+    }
+}
---
+++ SimplHelpers/SimplHelpers/CollectionsHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplHelpers
+{
+    public static class CollectionsHelpers
+    {
+        /// <summary>
+        /// Serializes enum to key => value structure where value of the enum is the key
+        /// It will disregard duplicate values
+        /// </summary>
+        /// <param name="enum"></param>
+        /// <returns></returns>
+        public static Dictionary<int, string> EnumToDictionary(Enum @enum)
+        {
+            var type = @enum.GetType();
+            return Enum.GetValues(type).Cast<int>().Distinct().ToDictionary(e => e, e => Enum.GetName(type, e));
+        }
+    }
+}
